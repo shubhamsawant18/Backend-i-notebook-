9b84047d91ab22991a3d2368240473931e5bff4d
shubhamsawant18 <shubhsawant124@gmail.com>
2024-01-22 22:15:14 +0530

    updating note

--- routes/notes.js
+++ routes/notes.js
@@ -50,7 +50,42 @@ router.post('/addnote', fetchuser, [
             res.status(500).json({error: "Internal Server Error"});
         }
     });
-
+    // ROUTE 3:Update an existing note using: POST "/api/notes/updatenote". Login required
+    router.put('/updatenote/:id', fetchuser, async (req, res) => {
+        const { title, description, tag } = req.body;
+    
+        // Create a newNote Object
+        const newNote = {};
+        if (title) newNote.title = title;
+        if (description) newNote.description = description;
+        if (tag) newNote.tag = tag;
+    
+        try {
+            // Find the note to be updated
+            let note = await Note.findById(req.params.id);
+            if (!note) {
+                return res.status(404).send("Note not found");
+            }
+    
+            // Check if the user is authorized to update the note
+            if (note.user.toString() !== req.user.id) {
+                return res.status(401).send("Not allowed");
+            }
+    
+            // Update the note and return the updated note
+            note = await Note.findByIdAndUpdate(
+                req.params.id,
+                { $set: newNote },
+                { new: true }
+            );
+    
+            res.json({ note });
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    });
+    
 
 
 module.exports = router
